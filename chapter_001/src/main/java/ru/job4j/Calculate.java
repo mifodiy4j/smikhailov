770fae96ad836373552bff53ecc8809e23bf79bf
Mikhailov Sergey <mifodiy15@gmail.com>
2017-07-20 09:00:22 +0300

    task 4087

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,6 +1,19 @@
 package ru.job4j;
 
+/**
+* Класс Calculate для вывода Hello World
+*@author smikhailov
+*@since 20.07.2017
+*@version 1
+*/
 public class Calculate {
+
+	/**
+	* Вывод строки в консоль
+	*@param arg - arg
+	*@return void
+	*@throws
+	*/
     public static void main(String[] args) {
         System.out.println("Hello World");
     }

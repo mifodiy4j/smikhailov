2806829a6c51467af1f803bb8a942f32c319337f
Mikhailov Sergey <mifodiy15@gmail.com>
2017-07-26 09:18:43 +0300

    task 189

---
+++ chapter_001/src/main/java/ru/job4j/max/Max.java
@@ -0,0 +1,21 @@
+package ru.job4j.max;
+
+/**
+    * Max.
+    *
+    *@author smikhailov
+    *@since 26.07.2017
+    *@version 1
+    */
+public class Max {
+
+	/**
+        * Max.
+        *@param
+        *@return
+        *@throws
+        */
+	public int max(int first, int second) {
+		return first >= second ? first : second;
+	}
+}

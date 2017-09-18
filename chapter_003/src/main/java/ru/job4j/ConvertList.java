21cdc27f0bc2e64bb4f87bb54dd40af277b9747b
Mikhailov Sergey <mifodiy15@gmail.com>
2017-09-18 09:08:49 +0300

    task 10035

---
+++ chapter_003/src/main/java/ru/job4j/ConvertList.java
@@ -0,0 +1,39 @@
+package ru.job4j;
+
+import java.util.*;
+
+public class ConvertList {
+
+    public List<Integer> toList (int[][] array) {
+        List<Integer> list = new ArrayList<>();
+
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                list.add(array[i][j]);
+            }
+        }
+        return list;
+    }
+
+    public int[][] toArray (List<Integer> list, int rows) {
+
+        int column = list.size() / rows + 1;
+        int[][] array = new int[rows][column];
+
+        System.out.println(column);
+
+        Iterator<Integer> iter = list.iterator();
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < column; j++) {
+                if (iter.hasNext()){
+                    array[i][j] = iter.next();
+                } else {
+                    array[i][j] = 0;
+                }
+            }
+        }
+
+        return array;
+    }
+}
---
+++ chapter_003/src/test/java/ru/job4j/ConvertListTest.java
@@ -0,0 +1,105 @@
+package ru.job4j;
+
+import ru.job4j.*;
+
+import java.util.*;
+
+import org.junit.Test;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+* Test.
+*
+* @author Sergey Mikhailov (mailto:mifodiy67@mail.ru)
+* @version $Id$
+* @since 0.1
+*/
+public class ConvertListTest {
+	/**
+	* Test convertList.
+	*/
+   @Test
+   public void whenListConvertToArray7Elements() {
+
+       ConvertList convertList = new ConvertList();
+
+       List<Integer> list = new ArrayList<>();
+       list.add(1);
+       list.add(2);
+       list.add(3);
+       list.add(4);
+       list.add(5);
+       list.add(6);
+       list.add(7);
+
+       int[][] array = {
+               {1, 2, 3},
+               {4, 5, 6},
+               {7, 0 ,0}
+       };
+
+       assertThat(convertList.toArray(list, 3), is(array));
+
+   }
+
+    /**
+     * Test convertList.
+     */
+    @Test
+    public void whenListConvertToArray10Elements() {
+
+        ConvertList convertList = new ConvertList();
+
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        list.add(6);
+        list.add(7);
+        list.add(8);
+        list.add(9);
+        list.add(10);
+
+        int[][] array = {
+                {1, 2, 3, 4},
+                {5, 6, 7, 8},
+                {9,10 ,0, 0}
+        };
+
+        assertThat(convertList.toArray(list, 3), is(array));
+
+    }
+
+    /**
+     * Test convertList.
+     */
+    @Test
+    public void whenArrayConvertToArray7Elements() {
+
+        ConvertList convertList = new ConvertList();
+
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        list.add(6);
+        list.add(7);
+
+        int[][] array = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7}
+        };
+
+        assertThat(convertList.toList(array), is(list));
+
+    }
+
+}

cdf60b92e82876c084b830c698f752f519b5788e
Mikhailov Sergey <mifodiy15@gmail.com>
2017-10-06 09:30:54 +0300

    task 151 second commit

--- chapter_005/src/main/java/ru/job4j/IteratorPrimeNumberArray.java
+++ chapter_005/src/main/java/ru/job4j/IteratorPrimeNumberArray.java
@@ -1,70 +1,44 @@
 package ru.job4j;
 
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 public class IteratorPrimeNumberArray implements Iterator {
 
     private final int[] value;
-    private int index = -1;
+    private int index = 0;
+    private int currentIndexEventNumber = 0;
 
     public IteratorPrimeNumberArray(int[] value) {
         this.value = value;
-        for (int i = 0; i < value.length; i++) {
-            boolean marker = true;
-            for (int d = 2; d * d < value[i]; d++) {
-                if (value[i] % d == 0) {
-                    marker = false;
-                }
-            }
-            if (marker) {
-                index = i;
-                break;
-            }
-        }
     }
 
     @Override
     public boolean hasNext() {
-        boolean status = false;
-        if (index != -1) {
-            for (int i = index; i < value.length; i++) {
-                boolean marker = true;
-                for (int d = 2; d * d < value[i]; d++) {
-                    if (value[i] % d == 0) {
-                        marker = false;
-                    }
-                }
-                if (marker) {
-                    status = true;
+        for (int i = index; i < value.length; i++) {
+            boolean markerCheckPrime = true;
+            for (int d = 2; d * d <= value[i]; d++) {
+                if (value[i] % d == 0) {
+                    markerCheckPrime = false;
                     break;
                 }
             }
+            if(markerCheckPrime) {
+                index = i;
+                currentIndexEventNumber = i;
+                return true;
+            }
         }
-        return status;
+        return false;
     }
 
     @Override
     public Object next() {
-        int oldIndex = index;
-        if (index != -1 && index != value.length -1) {
-            for (int i = index + 1; i < value.length; i++) {
-                boolean marker = true;
-                if (value[i] == 1) {
-                    marker = false;
-                }
-                for (int d = 2; d * d <= value[i]; d++) {
-                    if (value[i] % d == 0) {
-                        marker = false;
-                    }
-                }
-                if (marker) {
-                    index = i;
-                    break;
-                } else {
-                    index = -1;
-                }
-            }
-        } else {index = -1;}
-        return value[oldIndex];
+        if (hasNext()) {
+            index++;
+            return value[currentIndexEventNumber];
+        } else {
+            throw new NoSuchElementException();
+        }
     }
 }
--- chapter_005/src/test/java/ru/job4j/IteratorPrimeNumberArrayTest.java
+++ chapter_005/src/test/java/ru/job4j/IteratorPrimeNumberArrayTest.java
@@ -3,29 +3,41 @@ package ru.job4j;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.NoSuchElementException;
+
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
 public class IteratorPrimeNumberArrayTest {
-    @Test
-    public void whenGetNextCallShouldPointerForward() {
-        IteratorPrimeNumberArray it = new IteratorPrimeNumberArray(new int[] {3, 4, 5, 6, 7});
-
-        it.next();
-        it.next();
-        int result = (Integer) it.next();
-
-        Assert.assertThat(result, is(7));
-    }
-
     @Test
     public void whenCheckNextPositionShouldReturnContentValue() {
         IteratorPrimeNumberArray it = new IteratorPrimeNumberArray(new int[] {3, 4, 5, 6, 7});
 
         it.next(); //3
         it.next(); //5
+        int result = (Integer) it.next();
+
+        Assert.assertThat(result, is(7));
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void whenCheckNextPositionAfterEnd() {
+        IteratorPrimeNumberArray it = new IteratorPrimeNumberArray(new int[] {3, 4, 5, 6, 7});
+
+        it.next(); //3
+        it.next(); //5
         it.next(); //7
-        it.hasNext();
+        int result = (Integer) it.next(); //hasn't element
+
+    }
+
+    @Test
+    public void whenGetNextCallShouldPointerForward() {
+        IteratorPrimeNumberArray it = new IteratorPrimeNumberArray(new int[] {3, 4, 5, 6, 7});
+
+        it.next(); //3
+        it.next(); //5
+        it.next(); //7
         boolean result = it.hasNext();
 
         assertThat(result, is(false));

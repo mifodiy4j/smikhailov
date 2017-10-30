83d516e3582330d30d855902d69777f30f3307f7
Mikhailov Sergey <mifodiy15@gmail.com>
2017-10-30 11:00:50 +0300

    task 1005

--- chapter_005/src/test/java/ru/job4j/MapTest.java
+++ chapter_005/src/test/java/ru/job4j/MapTest.java
@@ -1,6 +1,12 @@
 package ru.job4j;
 
+import com.sun.org.apache.xpath.internal.SourceTree;
+import org.junit.Test;
+
 import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author smikhailov
@@ -17,5 +23,19 @@ public class MapTest {
             this.children = children;
             this.birthday = birthday;
         }
+    }
+
+    @Test
+    public  void map() {
+        Calendar c1 = new GregorianCalendar(1990, 12, 21);
+        User first = new User("Jack", 2, c1);
+        User second = new User("Jack", 2, c1);
+
+        Map<User, Object> map = new HashMap<>();
+        Object obj = new Object();
+        map.put(first, obj);
+        map.put(second, obj);
+        System.out.printf("%s %n",map);
+        System.out.printf("Size map = %d",map.size());
     }
 }

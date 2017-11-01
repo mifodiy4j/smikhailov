2c5b93913cce56d3cf7fe8be80342a808a50fce7
Mikhailov Sergey <mifodiy15@gmail.com>
2017-11-01 17:55:05 +0300

    task 1008 second commit

--- chapter_005/src/main/java/ru/job4j/Directory.java
+++ chapter_005/src/main/java/ru/job4j/Directory.java
@@ -5,36 +5,37 @@ import java.util.Arrays;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
-public class Directory<T, V> implements Iterable<T> {
+public class Directory<T, V> implements Iterable {
     private static final int DEFAULT_CAPACITY = 10;
     private static final float DEFAULT_LOAD_FACTOR = 0.75f;
-    protected Object[] catalog;
+    protected Node[] catalog = new Node[DEFAULT_CAPACITY];
 
-    public Directory() {
-        catalog = new Object[DEFAULT_CAPACITY];
-    }
 
     boolean insert(T key, V value) {
         int k = key.hashCode();
         int index = k % catalog.length;
-        catalog[index] = value;
+        catalog[index] = new Node<>(key, value);
 
-        /*int load = 0;
-        for(Object obj : catalog) {
-            if (obj != null) {
+        int load = 0;
+        for(Node node : catalog) {
+            if (node != null) {
                 load++;
             }
         }
 
         if (load >= catalog.length * DEFAULT_LOAD_FACTOR) {
-            Object[] temp = Arrays.copyOf(catalog, catalog.length);
-            catalog = new Object[catalog.length * 3 / 2 + 1];
+            Node[] temp = Arrays.copyOf(catalog, catalog.length);
+            catalog = new Node[catalog.length * 3 / 2 + 1];
             for (int i = 0; i < temp.length; i++) {
                 if (temp[i] != null) {
-                    catalog
+                    key = (T) temp[i].getKey();
+                    k = key.hashCode();
+                    index = k % catalog.length;
+                    value = (V) temp[i].getValue();
+                    catalog[index] = new Node<>(key, value);
                 }
             }
-        }*/
+        }
 
         return true;
     }
@@ -42,21 +43,19 @@ public class Directory<T, V> implements Iterable<T> {
     V get(T key) {
         int k = key.hashCode();
         int index = k % catalog.length;
-        return (catalog[index] != null) ? (V)catalog[index] : null;
+        return (catalog[index] != null) ? (V) catalog[index].getValue() : null;
     }
 
     boolean delete(T key) {
-        boolean deleteIsGood = false;
-        if (get(key) != null) {
-            insert(key, null);
-            deleteIsGood = true;
-        }
-        return deleteIsGood;
+        int k = key.hashCode();
+        int index = k % catalog.length;
+        catalog[index] = null;
+        return true;
     }
 
     @Override
-    public Iterator<T> iterator() {
-        return new Iterator<T>() {
+    public Iterator iterator() {
+        return new Iterator() {
             int cursor = 0;
             @Override
             public boolean hasNext() {
@@ -67,12 +66,32 @@ public class Directory<T, V> implements Iterable<T> {
             }
 
             @Override
-            public T next() {
+            public Node next() {
                 if (cursor >= catalog.length) {
                     throw new NoSuchElementException();
                 }
-                return (T)catalog[cursor++];
+                int prevCursor = cursor;
+                hasNext();
+                return catalog[prevCursor];
             }
         };
+    }
+
+    private class Node<T, V> {
+        T key;
+        V value;
+
+        public Node(T key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        public V getValue() {
+            return value;
+        }
+
+        public T getKey() {
+            return key;
+        }
     }
 }
--- chapter_005/src/test/java/ru/job4j/DirectoryTest.java
+++ chapter_005/src/test/java/ru/job4j/DirectoryTest.java
@@ -3,12 +3,11 @@ package ru.job4j;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.NoSuchElementException;
+
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
-/**
- * Created by SERG on 31.10.2017.
- */
 public class DirectoryTest {
 
     @Test
@@ -29,6 +28,25 @@ public class DirectoryTest {
     }
 
     @Test
+    public void whenInsertElementsMoreThenTheLoadFactorShouldReturnContentValueForTheKey() {
+
+        Directory<String, Integer> directory = new Directory<>();
+
+        directory.insert("0", 1000);
+        directory.insert("1", 1111);
+        directory.insert("2", 2222);
+        directory.insert("3", 3333);
+        directory.insert("4", 4444);
+        directory.insert("5", 5555);
+        directory.insert("6", 6666);
+        directory.insert("7", 7777);
+        directory.insert("8", 8888);
+
+        int result = directory.get("8");
+        Assert.assertThat(result, is(8888));
+    }
+
+    @Test
     public void whenInsertElementsWithDuplicateKeyShouldReturnContentValueWhichLastAdd() {
 
         Directory<String, Integer> directory = new Directory<>();
@@ -44,6 +62,24 @@ public class DirectoryTest {
 
         int result = directory.get("6");
         Assert.assertThat(result, is(6));
+    }
+
+    @Test (expected = NullPointerException.class)
+    public void whenDeleteElementShouldReturn() {
+
+        Directory<String, Integer> directory = new Directory<>();
+
+        directory.insert("0", 1000);
+        directory.insert("1", 1111);
+        directory.insert("2", 2222);
+        directory.insert("3", 3333);
+        directory.insert("4", 4444);
+        directory.insert("5", 5555);
+        directory.insert("6", 6666);
+        directory.delete("2");
+
+        int result = directory.get("2");
+        Assert.assertThat(result, is(2222));
     }
 
 }
